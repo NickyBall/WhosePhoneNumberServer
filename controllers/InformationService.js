faae1a8ba38e91e0055ac13b5568b7fe46243bae
NickyBall <ball.vong.yo.gh@gmail.com>
2017-08-17 23:55:23 +0700

    Setup Mongodb to Nodejs

--- controllers/InformationService.js
+++ controllers/InformationService.js
@@ -4,6 +4,25 @@ exports.getAllAreaCode = function(args, res, next) {
   /**
    * parameters expected in the args:
   **/
+  var mongo = require('mongodb');
+  var mongoClient = mongo.MongoClient;
+  var url = 'mongodb://localhost:27017/wpn';
+
+  mongoClient.connect(url, function(err, db){
+    if (err) {
+      console.log('Unable to Connect Server.');
+    } else {
+      console.log('Connect Establish.');
+      var collection = db.collection('AreaCode');
+      collection.find({}).toArray(function (err, result){
+        if (err) {
+          res.send(err);
+        } else if (result.length) {
+          res.send(result);
+        }
+      });
+    }
+  });
     var examples = {};
   examples['application/json'] = [ {
   "code" : "201",
