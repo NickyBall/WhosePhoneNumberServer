d3b84142b303ab1238be5890a58995e16651c923
NickyBall <ball.vong.yo.gh@gmail.com>
2017-08-21 23:41:11 +0700

    Information with numverify api

--- controllers/InformationService.js
+++ controllers/InformationService.js
@@ -70,25 +70,70 @@ exports.getPhoneDescription = function(args, res, next) {
    * parameters expected in the args:
   * phonenumber (String)
   **/
-    var examples = {};
-  examples['application/json'] = [ {
-  "country" : "United State",
-  "carrier" : "VERIZON NEW YORK, INC.",
-  "city" : "Jersey City",
-  "usage_type" : "Landline",
-  "prefix" : "349",
-  "interformat" : "+1 718 349 9300",
-  "county" : "Queens",
-  "id" : "d290f1ee-6c54-4b01-90e6-d701748f0851",
-  "state" : "New Jersey",
-  "areacode" : "718"
-} ];
-  if(Object.keys(examples).length > 0) {
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
-  }
-  else {
-    res.end();
-  }
+  var phonenumber = args.phonenumber.value;
+  var country_code = 'US';
+  var format = '1';
+  var key = 'b8181f19a13dab009d378af83f20b019';
+
+  var mongo = require('mongodb');
+  var mongoClient = mongo.MongoClient;
+  var url = 'mongodb://localhost:27017/wpn';
+
+  mongoClient.connect(url, function(err, db){
+    if (err) {
+      var json = { "status" : "ERROR", "desc" : 'Unable to Connect Server.' };
+      db.close();
+      res.end(JSON.stringify(json));
+    } else {
+      console.log(phonenumber.replace("-",""));
+      var collection = db.collection('PhoneInfo');
+      collection.findOne({"local_format":phonenumber.replace("-","")}, (err, result) => {
+        if (err) {
+          db.close();
+          var json = { "status" : "ERROR", "desc" : err };
+          res.end(JSON.stringify(json));
+        } else {
+          if (result) {
+            db.close();
+            // var json = { "status" : "OK", "desc" : result };
+            res.end(JSON.stringify(result));
+          } else {
+            var request = require('request');
+
+            request('http://apilayer.net/api/validate?access_key='+key+'&number='+phonenumber.replace("-","")
+            + '&country_code='+country_code+'&format='+format, (error, response, body) => {
+                // console.log(res);
+                // console.log(body);
+                var resJson = JSON.parse(body);
+                var collection = db.collection('PhoneInfo');
+                collection.insertOne(resJson, function (err, result){
+                  if (err) {
+                    db.close();
+                    var json = { "status" : "ERROR", "desc" : err };
+                    res.end(JSON.stringify(json));
+                  } else {
+                    db.close();
+                    var json = { "status" : "OK", "desc" : result };
+                    res.end(JSON.stringify(resJson));
+                  }
+                });
+            });
+              // fetch('http://apilayer.net/api/validate?access_key=b8181f19a13dab009d378af83f20b019&number=4158586273&country_code=US&format=1').then(
+              //   response => response.json()
+              // ).then(response => {
+              //
+              // })
+
+          }
+
+        }
+      });
+    }
+
+  });
+
+
+
+
 
 }
