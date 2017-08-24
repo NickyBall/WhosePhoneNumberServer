7598208bf3593da609c27dce193df307f733b5f3
NickyBall <ball.vong.yo.gh@gmail.com>
2017-08-24 20:44:18 +0700

    GetRecentChecked

--- controllers/RecentlyService.js
+++ controllers/RecentlyService.js
@@ -4,19 +4,31 @@ exports.getRecentlyCheckedPhoneNumber = function(args, res, next) {
   /**
   * parameters expected in the args:
   **/
-  var examples = {};
-  examples['application/json'] = [ {
-    "address" : "Atlanta, Georgia, United States",
-    "phonenumber" : "404-275-4585",
-    "id" : "d290f1ee-6c54-4b01-90e6-d701748f0851"
-  } ];
-  if(Object.keys(examples).length > 0) {
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
-  }
-  else {
-    res.end();
-  }
+  var mongo = require('mongodb');
+  var mongoClient = mongo.MongoClient;
+  var url = 'mongodb://localhost:27017/wpn';
+
+  mongoClient.connect(url, function(err, db){
+    if (err) {
+      var json = { "status" : "ERROR", "desc" : 'Unable to Connect Server.' };
+      db.close();
+      res.end(JSON.stringify(json));
+    } else {
+      var collection = db.collection('Checked');
+      collection.find({}).sort({_id:-1}).limit(5).toArray( (err, result) => {
+        if (err) {
+          db.close();
+          var json = { "status" : "ERROR", "desc" : err };
+          res.end(JSON.stringify(json));
+        } else {
+          db.close();
+          // var json = { "status" : "OK", "desc" : result };
+          res.end(JSON.stringify(result));
+        }
+      });
+    }
+
+  });
 
 }
 
@@ -24,21 +36,6 @@ exports.getRecentlyComments = function(args, res, next) {
   /**
   * parameters expected in the args:
   **/
-  // var examples = {};
-  // examples['application/json'] = [ {
-  //   "date" : "2016-08-29T09:12:33.001Z",
-  //   "phonenumber" : "404-275-4585",
-  //   "comment" : "This is a debt collector",
-  //   "id" : "d290f1ee-6c54-4b01-90e6-d701748f0851",
-  //   "username" : "Widget Adapter"
-  // } ];
-  // if(Object.keys(examples).length > 0) {
-  //   res.setHeader('Content-Type', 'application/json');
-  //   res.end(JSON.stringify(examples[Object.keys(examples)[0]] || {}, null, 2));
-  // }
-  // else {
-  //   res.end();
-  // }
   var mongo = require('mongodb');
   var mongoClient = mongo.MongoClient;
   var url = 'mongodb://localhost:27017/wpn';
